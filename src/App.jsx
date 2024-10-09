0bb641ba5bdeec310a434e6bf4cef9546f329b66
janidu <jchamoth12@gmail.com>
2024-10-09 02:14:47 -0700

    design the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
+import MenuItem from './components/Menu/Menuitem';
 import Navbar from './components/Navbar/Navbar';
+import Heading from './components/Header/Header';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Heading/>
+      <MenuItem/>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import SubHeading from '../SubHeading/SubHeading';
+
+export default function Header() {
+  return (
+    <div className='app_header app_wrapper section_padding' id='home'>
+        <div className='app_wrapper_info'>
+            <SubHeading/>
+        </div>
+    </div>
+  )
+}
